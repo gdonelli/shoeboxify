e3ca9a589415a403f0903c47eb02cb322eb2f49b
Giovanni Donelli <giovanni.donelli@gmail.com>
2012-10-26 11:08:56 -0700

    add new env var

--- jitsu-setenv.sh
+++ jitsu-setenv.sh
@@ -2,4 +2,8 @@
 
 jitsu env set APP_ID $APP_ID
 jitsu env set APP_SECRET $APP_SECRET
+
 jitsu env set SESSION_SECRET $SESSION_SECRET
+jitsu env set SESSION_DB_URL $SESSION_DB_URL
+jitsu env set SESSION_DB_NAME $SESSION_DB_NAME
+
