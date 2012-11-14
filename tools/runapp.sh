81b684314dee2fb77e5875930e161861ec6d1b48
Giovanni Donelli <giovanni.donelli@gmail.com>
2012-11-14 10:08:59 -0800

    added private folder

---
+++ tools/runapp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ./setenv.sh
+
+# if [ "$#" -gt 0 ]; then
+# 	echo "Setting up variable in Nodejitsu"
+#   	cd shoeboxify
+# 	sh ./jitsu-setenv.sh
+# 	cd ..
+# else
+
+supervisor -p 1000 ../app/app.js	
+
